be63d6a7bf0cc14ccf36b85be76e5f6f30b1bc2f
Adrian Igras <48068613+advvix@users.noreply.github.com>
2019-12-09 14:10:57 +0100

    Update lista 5.py

--- lista 5.py
+++ lista 5.py
@@ -19,8 +19,9 @@ upz = 0.0
 mousex = windowWidth / 2
 mousey = windowHeight / 2
 global stat1
-
 kordy = []
+cube_select = 0
+
 
 def mouseMotion(x, y):
     global mousex, mousey
@@ -59,22 +60,17 @@ def paint():
         (1, 0, 0),
         (0, 1, 0),
         (0, 0, 1),
-        (1, 1, 1),
+        (0, 0, 0),
         (1, 0, 1),
         (1, 1, 0),
         (0, 1, 1),
-        (1,0.5,0),
-        (0.5,1,0),
-        (0.5,1,0.5),
-        (0.5,0,0.5)
+        (1, 0.5, 0),
+        (0.5, 1, 0),
+        (0.5, 1, 0.5),
+        (0.5, 0, 0.5)
     )
 
-
-
-
-
-
-    def Cube(mousex, mousey, color,size):
+    def Cube(mousex, mousey, color, size, cube_number):
         global kej
         glBegin(GL_QUADS)
         glColor3f(colors[color][0], colors[color][1], colors[color][2])
@@ -82,10 +78,10 @@ def paint():
         mousex = (windowWidth / 2 - mousex) / 130
         mousey = -(windowHeight / 2 - mousey) / 20
         """górna ściana"""
-        glVertex3f(mousex + size, mousey + size, 1.0-size)  # front upper right
-        glVertex3f(mousex - size, mousey + size, 1.0-size)  # front upper left
-        glVertex3f(mousex - size, mousey + size, 1.0+size)  # back upper left
-        glVertex3f(mousex + size, mousey + size, 1.0+size)  # back upper right
+        glVertex3f(mousex + size, mousey + size, 1.0 - size)  # front upper right
+        glVertex3f(mousex - size, mousey + size, 1.0 - size)  # front upper left
+        glVertex3f(mousex - size, mousey + size, 1.0 + size)  # back upper left
+        glVertex3f(mousex + size, mousey + size, 1.0 + size)  # back upper right
         """tylnia ściana"""
         glVertex3f(mousex - size, mousey + size, 1.0 + size)  # back upper left
         glVertex3f(mousex + size, mousey + size, 1.0 + size)  # back upper right
@@ -94,26 +90,29 @@ def paint():
         """dolna ściana"""
         glVertex3f(mousex + size, mousey - size, 1.0 + size)  # back lower right
         glVertex3f(mousex - size, mousey - size, 1.0 + size)  # back lower left
-        glVertex3f(mousex - size, mousey - size, 1.0-size)  # front lower left
-        glVertex3f(mousex + size, mousey - size, 1.0-size)  # fron lower right
+        glVertex3f(mousex - size, mousey - size, 1.0 - size)  # front lower left
+        glVertex3f(mousex + size, mousey - size, 1.0 - size)  # fron lower right
         """przednia ściana"""
-        glVertex3f(mousex + size, mousey - size, 1.0-size)  # front lower right
-        glVertex3f(mousex - size, mousey - size, 1.0-size)  # front lower left
-        glVertex3f(mousex - size, mousey + size, 1.0-size)  # front upper left
-        glVertex3f(mousex + size, mousey + size, 1.0-size)  # front upper right
+        glVertex3f(mousex + size, mousey - size, 1.0 - size)  # front lower right
+        glVertex3f(mousex - size, mousey - size, 1.0 - size)  # front lower left
+        glVertex3f(mousex - size, mousey + size, 1.0 - size)  # front upper left
+        glVertex3f(mousex + size, mousey + size, 1.0 - size)  # front upper right
         """lewa ściana"""
-        glVertex3f(mousex + size, mousey + size, 1.0-size)  # front upper right
-        glVertex3f(mousex + size, mousey - size, 1.0-size)  # front lower right
+        glVertex3f(mousex + size, mousey + size, 1.0 - size)  # front upper right
+        glVertex3f(mousex + size, mousey - size, 1.0 - size)  # front lower right
         glVertex3f(mousex + size, mousey - size, 1.0 + size)  # back lower right
         glVertex3f(mousex + size, mousey + size, 1.0 + size)  # back upper right
         """prawa ściana"""
-        glVertex3f(mousex - size, mousey + size, 1.0-size)  # front upper left
-        glVertex3f(mousex - size, mousey - size, 1.0-size)  # front lower left
+        glVertex3f(mousex - size, mousey + size, 1.0 - size)  # front upper left
+        glVertex3f(mousex - size, mousey - size, 1.0 - size)  # front lower left
         glVertex3f(mousex - size, mousey - size, 1.0 + size)  # back lower left
         glVertex3f(mousex - size, mousey + size, 1.0 + size)  # back upper left
-
-
         glEnd()
+        text = f'Cube nr:{cube_number+1}'
+        glColor3f(0, 0, 0)
+        glRasterPos3f(mousex, mousey,-1.5)
+        for ch in text:
+            glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, ctypes.c_int(ord(ch)))
         kej = 0
 
         # testowe
@@ -123,35 +122,57 @@ def paint():
         # glVertex3f(1.0, -1.0, 1.0) # lower right
         # glEnd()
 
-    # czerwony trójkąt
     losu_losu = np.random.randint(0, 11)
-    losu_losu2 = np.random.uniform(0.05, 2)
+    losu_losu2 = np.random.uniform(0.2, 2)
 
     if kej == 1:
-        kordy.append((mousex,mousey,losu_losu,losu_losu2))
+        kordy.append((mousex, mousey, losu_losu, losu_losu2))
         # print(((windowHeight / 2 - mousey)/20))
 
     for i in range(len(kordy)):
-        Cube(kordy[i][0],kordy[i][1],kordy[i][2],kordy[i][3])
+        Cube(kordy[i][0], kordy[i][1], kordy[i][2], kordy[i][3],i)
 
 
-    # zielony prostokąt
-    glColor3f(0.0, 1.0, 0.0)
-    glBegin(GL_QUADS)
-    glVertex(2.0, 0.0, 0.0)
-    glVertex(3.0, 0.0, 0.0)
-    glVertex(3.0, 1.0, 0.0)
-    glVertex(2.0, 1.0, 0.0)
-    glEnd()
-    # niebieski wielokąt
-    glColor3f(0.0, 0.0, 1.0)
-    glBegin(GL_QUADS)
-    glVertex(-3.0, 0.0, 0.0)
-    glVertex(-2.0, 1.0, 0.0)
-    glVertex(-3.0, 2.0, 0.0)
-    glVertex(-4.0, 2.0, 0.0)
-    glVertex(-4.0, 1.0, 0.0)
-    glEnd()
+
+
+##################### ZMIANY PO ITERACJI HERE
+
+    text = f'Select cube:{cube_select}'
+    glColor3f(0, 0, 0)
+    glRasterPos3f(0, 2,-2.5)
+    for ch in text:
+        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, ctypes.c_int(ord(ch)))
+
+    text = f'Change cube: +/-'
+    glColor3f(0, 0, 0)
+    glRasterPos3f(0, 2,-2.4)
+    for ch in text:
+        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_10, ctypes.c_int(ord(ch)))
+    text = f'Change color: k | Delete cube: l'
+    glColor3f(0, 0, 0)
+    glRasterPos3f(0, 2,-2.3)
+    for ch in text:
+        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_10, ctypes.c_int(ord(ch)))
+
+
+
+    # # zielony prostokąt
+    # glColor3f(0.0, 1.0, 0.0)
+    # glBegin(GL_QUADS)
+    # glVertex(2.0, 0.0, 0.0)
+    # glVertex(3.0, 0.0, 0.0)
+    # glVertex(3.0, 1.0, 0.0)
+    # glVertex(2.0, 1.0, 0.0)
+    # glEnd()
+    # # niebieski wielokąt
+    # glColor3f(0.0, 0.0, 1.0)
+    # glBegin(GL_QUADS)
+    # glVertex(-3.0, 0.0, 0.0)
+    # glVertex(-2.0, 1.0, 0.0)
+    # glVertex(-3.0, 2.0, 0.0)
+    # glVertex(-4.0, 2.0, 0.0)
+    # glVertex(-4.0, 1.0, 0.0)
+    # glEnd()
     # celownik
     glColor(0.0, 0.0, 0.0)
     glPushMatrix()
@@ -173,15 +194,33 @@ def paint():
     glutSwapBuffers()
     pass
 
-
 kej = 0
-
 ##############################
+def color_cube(cube_select):
+    if cube_select <= 0:
+        pass
+    elif cube_select >= len(kordy):
+        pass
+    else:
+        kolorki = np.random.uniform(0,11)
+        kordy[cube_select-1] = list(kordy[cube_select-1])
+        kordy[cube_select-1][2] = int(kolorki)
+        kordy[cube_select-1] = tuple(kordy[cube_select-1])
+
+def del_cube(cube_select):
+    if cube_select <= 0:
+        pass
+    else:
+        kolorki = np.random.uniform(0,11)
+        kordy[cube_select-1] = list(kordy[cube_select-1])
+        del kordy[cube_select-1]
+
 def keyboard(bkey, x, y):
     global mousex
     global lookz
     global mousey
     global kej
+    global cube_select
     key = bkey.decode("utf-8")
     if key == 'd':
         mousex += 10
@@ -191,21 +230,32 @@ def keyboard(bkey, x, y):
         mousey += 5
     elif key == 's':
         mousey -= 5
-
-
+    elif key == '+':
+        if cube_select >= len(kordy):
+            pass
+        else:
+            cube_select += 1
+    elif key == '-':
+        if cube_select < 1:
+            pass
+        else:
+            cube_select -= 1
+    elif key == 'k':
+        color_cube(cube_select)
+    elif key == 'l':
+        del_cube(cube_select)
     elif key == '1':
         kej = 1
-
         # ESC HERE
     elif key == '\x1b':
         sys.exit()
     elif kej == '':
         kej = 0
 
+
 ##############################
 
 while True:
-
     # utworzenie okna
     glutInit(sys.argv)
     glutInitWindowPosition(int((ctypes.windll.user32.GetSystemMetrics(0) - windowWidth) / 2),
